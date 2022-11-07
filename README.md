259fb6c660720750c219b8508a6968b5dc4700f8
AndRBuZ <andrrrw56@gmail.com>
2022-11-07 16:19:56 +0500

    add lesson2 task

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # README
+Entities:
+
+* User
+
+* Admin
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Ekaterinburg'
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
