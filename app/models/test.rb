861e48a33e702906518310d4f564bdb4b9061e1a
AndRBuZ <andrrrw56@gmail.com>
2022-11-13 21:43:16 +0500

    Feat: add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :users_tests
+  has_many :users, through: :user_tests
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
   def self.sorted_descending_with_category(title_category)
     joins('JOIN categories ON tests.category_id = categories.id')
     .where(categories: { title: title_category })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_tests
+  has_many :tests, through: :user_tests
+
   def complied_tests_on_level(level)
     Test.('JOIN user_tests ON tests.id = user_tests.test_id')
         .where(user_tests: { user_id: id }, level: level)
