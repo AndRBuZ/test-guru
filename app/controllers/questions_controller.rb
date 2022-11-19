d3d60bf09779c414b57db52614edb7c11c8323ae
AndRBuZ <andrrrw56@gmail.com>
2022-11-19 17:55:16 +0500

    Fix: controller, routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show edit destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -8,25 +9,34 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    render plain: @test.questions.find(params[:id]).text
+    render plain: @question.text
   end
 
   def new; end
 
   def create
-    @test.questions.new(question_params).save
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   def edit; end
 
   def destroy
-    @test.questions.find(params[:id]).destroy
+    @question.destroy
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
