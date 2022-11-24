87931bdc705507a08f606c3655c4b2bb17105a43
AndRBuZ <andrrrw56@gmail.com>
2022-11-24 19:25:59 +0500

    Lesson 9

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,25 @@
 class TestsController < ApplicationController
+  before_action :set_test, only: %i[show start]
+  before_action :set_user, only: :start
+
   def index
     @tests = Test.all
   end
 
-  def show
+  def show; end
+
+  def start
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
+
+  private
+
+  def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_user
+    @user = User.last
   end
 end
---
+++ app/models/test_passage.rb
@@ -0,0 +1,51 @@
+class TestPassage < ApplicationRecord
+  belongs_to :user
+  belongs_to :test
+  belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_questions, on: :update
+
+  def complited?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answers?(answer_ids)
+    save!
+  end
+
+  def succeeded?
+    correct_percent >= 85
+  end
+
+  def correct_percent
+    correct_questions.to_i * 100 / test.questions.count
+  end
+
+  def current_question_number
+    test.questions.index(current_question) + 1
+  end
+
+  private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_questions
+    self.current_question = next_question
+  end
+
+  def correct_answers?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+end
