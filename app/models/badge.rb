23ee8f07579a3948d001341aa192b3cdaec8dff8
AndRBuZ <andrrrw56@gmail.com>
2023-02-01 19:11:57 +0500

    Fix badge model, badge service

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,8 +1,8 @@
 class Badge < ApplicationRecord
-  has_many :user_badges
+  RULES = %i[first level category].freeze
+
+  has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
   validates :title, :img_name, :rule, presence: true
-
-  RULES = %i[first level category].freeze
 end
